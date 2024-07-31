f07c204c3e854ebdbd6ecb42f8d0146873e35ca4
LucasDowiak <lcdowi@gmail.com>
2024-07-31 12:57:33 -0400

    add summary mehtod

---
+++ R/summary_method.R
@@ -0,0 +1,127 @@
+#' Summary Method
+#' 
+#' Summarize the fitted parameters of the HME
+#' 
+#' @param obj An S3 object of class "hme"
+#' 
+#' @param vcv The type of variance-covariance matrix to use for standard errors.
+#'            Defaults to the outer product of the gradient (OPG)
+#'
+#' @param type Select which nodes of the HME to provide summary reports
+#' 
+#' @export
+#' 
+summary.hme <- function(obj,
+                        vcv=c("OPG", "hessian", "sandwich"),
+                        type=c("all", "experts", "gates"))
+{
+  vcv <- match.arg(vcv)
+  type <- match.arg(type)
+  gatebool <- expertbool <- FALSE
+  if (type == "all") {
+    gatebool <- expertbool  <- TRUE
+  } else if (type == "gates") {
+    gatebool <- TRUE
+  } else if (type == "experts") {
+    expertbool <- TRUE
+  }
+  
+  # Grab the specified variance-covariance matrix
+  if (vcv == "OPG") {
+    std_errs <- sqrt(diag(solve(obj$vcv[[vcv]])))
+  } else if (vcv == "hessian") {
+    stop("Hessian var-cov not implemented")
+    std_errs <- sqrt(diag(-solve(obj$vcv[[vcv]])))
+  } else {
+    stop("Sandwich var-cov not implemented")
+    std_errs <- sqrt(diag(obj$vcv[[vcv]]))
+  }
+  
+  
+  # the std_errs vector is not broken up as a list of gate or expert nodes
+  # so we must do it before summarizing
+  
+  # Gate std_errs
+  depth <- max(sapply(strsplit(obj[["expert.nms"]], "\\."), length))
+  lgpn <- length(obj[["gate.pars.nms"]])
+  
+  # What kind of MoE is it
+  wide_hme <- depth == 2 && length(obj[["expert.nms"]]) > 2
+  
+  # Wide HME
+  if (wide_hme) {
+    lgp <- length(obj[["gate.pars"]][[1]])
+    ngates <- lgp * lgpn
+    gate.errs <- std_errs[1:ngates]
+    gate.errs <- list(split(gate.errs, ((seq_along(gate.errs) - 1) %/% lgpn) + 1))
+  } else {
+    lgp <- length(obj[["gate.pars"]])
+    ngates <- lgp * lgpn
+    gate.errs <- std_errs[1:ngates]
+    gate.errs <- split(gate.errs, ((seq_along(gate.errs) - 1) %/% lgpn) + 1)
+  }
+  names(gate.errs) <- names(obj[["gate.pars"]])
+  
+  # Expert std_errs
+  lep <- length(obj[["expert.pars"]])
+  lepn <- length(obj[["expert.pars.nms"]])
+  expert.errs <- std_errs[(ngates + 1):length(std_errs)]
+  expert.errs <- split(expert.errs, ((seq_along(expert.errs) - 1) %/% lepn))
+  names(expert.errs) <- names(obj[["expert.pars"]])
+  
+  
+  cat(sprintf("Log-Likelihood: %f", logLik(obj)))
+  if (expertbool) {
+    
+    for (e in names(obj[["expert.pars"]])) {
+      
+      gpp <- gate_path_product("0", e, obj$list_priors)
+      share <- sum(gpp) / obj$N
+      
+      cat(sprintf("\n--------------------------\nexpert-%s \t share:  %.3f\n\n", e, share))
+      
+      p <- obj[["expert.pars"]][[e]]
+      se <- expert.errs[[e]]
+      zstat <- p / se
+      tbl <- cbind(p, se, zstat, 2 * pnorm(-abs(zstat)))
+      dimnames(tbl) <- list(obj[['expert.pars.nms']],
+                            c("Estimate", "Std.Err", "z-stat", "Pr(>|z|)"))
+      printCoefmat(tbl, digits = 3)
+    }
+  }
+
+  cat("\n\n----------------------------------------------------\n\n")
+  
+  if (gatebool) {
+    
+    if (wide_hme) {
+      pars <- obj[["gate.pars"]][["0"]]
+      cat(sprintf("\n--------------------------\ngate-%s\n", "0"))
+      for (g in seq_along(pars)) {
+        p <- pars[[g]]
+        se <- gate.errs[["0"]][[g]]
+        zstat <- p / se
+        tbl <- cbind(p, se, zstat, 2 * pnorm(-abs(zstat)))
+        dimnames(tbl) <- list(obj[['gate.pars.nms']],
+                              c("Estimate", "Std.Err", "z-stat", "Pr(>|z|)"))
+        printCoefmat(tbl, digits = 3)
+        cat("\n")
+      }
+    } else {
+      pars <- obj[["gate.pars"]]
+      for (g in names(pars)) {
+        cat(sprintf("\n--------------------------\ngate-%s\n", g))
+        p <- pars[[g]]
+        se <- gate.errs[[g]]
+        zstat <- p / se
+        tbl <- cbind(p, se, zstat, 2 * pnorm(-abs(zstat)))
+        dimnames(tbl) <- list(obj[['gate.pars.nms']],
+                              c("Estimate", "Std.Err", "z-stat", "Pr(>|z|)"))
+        printCoefmat(tbl, digits = 3)
+        cat("\n")
+      }
+    }
+  }
+  
+  cat("\n\n----------------------------------------------------\n\n")
+}
